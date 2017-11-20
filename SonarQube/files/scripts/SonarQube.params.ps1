268a23049d7b6238ea5129d137e7f1d61bdeb48f
Anton Kuryan <a.kuryan@colours.nl>
2017-11-20 16:07:45 +0300

    Trying to fix parsing of parameters

--- SonarQube/files/scripts/SonarQube.params.ps1
+++ SonarQube/files/scripts/SonarQube.params.ps1
@@ -1,19 +1,20 @@
 param(
-    [string] $params,
     [string] $sqScannerRoot
 )
 
 #This script is designed to replace replace '-D' with '/d:' to maintain compatibility with official Teamcity SonarQube runner
 
+#Update path to hold path to scanner
+$path = (Get-Item Env:\PATH).Value;
+#just in case our PATH already ends with semicolon
+$path = $path.Trimend(';')
+$path = $path + ";$sqScannerRoot;"
+Write-Host "PATH: $path"
+Write-Host "##teamcity[setParameter name='env.PATH' value='$path']"
+$params = (Get-Item Env:\sonarParameters).Value;
 if (![string]::IsNullOrEmpty($params))
 {
   $changedParams = $params.Replace("-D","/d:").replace("`n"," ").replace("`r"," ")
   Write-Host "changedParams: $changedParams"
   Write-Host "##teamcity[setParameter name='env.sonar.parameters' value='$changedParams']"
 }
-
-#Update path to hold path to scanner
-$path = (Get-Item Env:\PATH).Value;
-$path = $path + ";$sqScannerRoot"
-Write-Host "PATH: $path"
-Write-Host "##teamcity[setParameter name='env.PATH' value='$path']"
