9ad29e7b981dc5477d693a8d808a0d8ca8f3230c
Anton Kuryan <a.kuryan@colours.nl>
2017-11-20 14:23:53 +0300

    Changed parameters propagation between build steps

--- SonarQube/files/scripts/SonarQube.params.ps1
+++ SonarQube/files/scripts/SonarQube.params.ps1
@@ -9,7 +9,7 @@ if (![string]::IsNullOrEmpty($params))
 {
   $changedParams = $params.Replace("-D","/d:").replace("`n"," ").replace("`r"," ")
   Write-Host "changedParams: $changedParams"
-  Write-Host "##teamcity[setParameter name='sonar.parameters' value='$changedParams']"
+  Write-Host "##teamcity[setParameter name='env.sonar.parameters' value='$changedParams']"
 }
 
 #Update path to hold path to scanner
