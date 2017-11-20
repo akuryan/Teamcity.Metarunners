2aa01f3167626c4843dc76aed71b1f196a90f049
Anton Kuryan <a.kuryan@colours.nl>
2017-11-20 13:53:50 +0300

    Path shall be updated at SonarQube.params.ps1, so - separate cmd step removed

--- SonarQube/files/scripts/SonarQube.params.ps1
+++ SonarQube/files/scripts/SonarQube.params.ps1
@@ -1,5 +1,6 @@
 param(
-    [string] $params
+    [string] $params,
+    [string] $sqScannerRoot
 )
 
 #This script is designed to replace replace '-D' with '/d:' to maintain compatibility with official Teamcity SonarQube runner
@@ -7,7 +8,12 @@ param(
 if (![string]::IsNullOrEmpty($params))
 {
   $changedParams = $params.Replace("-D","/d:").replace("`n"," ").replace("`r"," ")
-  "##teamcity[setParameter name='sonar.parameters' value='$changedParams']"
+  Write-Host "changedParams: $changedParams"
+  Write-Host "##teamcity[setParameter name='sonar.parameters' value='$changedParams']"
 }
 
-
+#Update path to hold path to scanner
+$path = (Get-Item Env:\PATH).Value;
+$path = $path + ";$sqScannerRoot"
+Write-Host "PATH: $path"
+Write-Host "##teamcity[setParameter name='env.PATH' value='$path']"
