263d826c6ed4f54241a849fd3aa54935395bf6d0
Anton Kuryan <a.kuryan@colours.nl>
2017-11-20 17:46:12 +0300

    Added one more check to ensure that less rattle will be in output

--- SonarQube/files/scripts/SonarQube.params.ps1
+++ SonarQube/files/scripts/SonarQube.params.ps1
@@ -4,13 +4,16 @@ param(
 
 #This script is designed to replace replace '-D' with '/d:' to maintain compatibility with official Teamcity SonarQube runner
 
-#Update path to hold path to scanner
-$path = (Get-Item Env:\PATH).Value;
-#just in case our PATH already ends with semicolon
-$path = $path.Trimend(';')
-$path = $path + ";$sqScannerRoot;"
-Write-Host "PATH: $path"
-Write-Host "##teamcity[setParameter name='env.PATH' value='$path']"
+if (![string]::IsNullOrEmpty($sqScannerRoot)) {
+  #Update path to hold path to scanner
+  $path = (Get-Item Env:\PATH).Value;
+  #just in case our PATH already ends with semicolon
+  $path = $path.Trimend(';')
+  $path = $path + ";$sqScannerRoot;"
+  Write-Host "PATH: $path"
+  Write-Host "##teamcity[setParameter name='env.PATH' value='$path']"
+}
+
 $params = (Get-Item Env:\sonarParameters).Value;
 if (![string]::IsNullOrEmpty($params)) {
   if ($params.Contains("-D")) {
