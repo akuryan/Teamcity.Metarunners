a1e9b65818b2498e30c092da05baea75a43b1fcc
Anton Kuryan <a.kuryan@colours.nl>
2017-11-20 17:00:58 +0300

    Fixed the way parameters are updated

--- SonarQube/files/scripts/SonarQube.params.ps1
+++ SonarQube/files/scripts/SonarQube.params.ps1
@@ -12,9 +12,19 @@ $path = $path + ";$sqScannerRoot;"
 Write-Host "PATH: $path"
 Write-Host "##teamcity[setParameter name='env.PATH' value='$path']"
 $params = (Get-Item Env:\sonarParameters).Value;
-if (![string]::IsNullOrEmpty($params))
-{
-  $changedParams = $params.Replace("-D","/d:").replace("`n"," ").replace("`r"," ")
+if (![string]::IsNullOrEmpty($params)) {
+  if ($params.Contains("-D")) {
+    $paramsCollection = $params -split "-D"
+    $changedParams = ""
+    foreach ($p in $paramsCollection) {
+      if ([string]::IsNullOrEmpty($p)) { continue }
+      $p = "/d:" + $p
+      $changedParams += $p
+    }
+  }
+  else {
+    $changedParams = $params
+  }
   Write-Host "changedParams: $changedParams"
   Write-Host "##teamcity[setParameter name='env.sonar.parameters' value='$changedParams']"
 }
