41ec08badec1efba8f47cbcc516d867dc884ae16
Anton Kuryan <anton.kuryan@colours.nl>
2018-11-09 17:15:56 +0300

    Fixed Azure auth

--- Clouds/Azure/AzureCostsSaver/files/scripts/budget-saver.ps1
+++ Clouds/Azure/AzureCostsSaver/files/scripts/budget-saver.ps1
@@ -1,11 +1,13 @@
 #this script is executed as inline script in metarunner
 
 #authorize at Azure
-$creds = New-Object System.Management.Automation.PSCredential (%azure.ApplicationId%, %azure.ApplicationSecret%);
-Login-AzureRmAccount -Credential $creds -ServicePrincipal -TenantId %azure.TenantId%;
-Select-AzureRmSubscription -SubscriptionId %azure.SubscriptionId%
+$applicationId = "%azure.ApplicationId%";
+$securePassword = "%azure.ApplicationSecret%";
+$credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $applicationId, $securePassword;
+Login-AzureRmAccount -Credential $credential -ServicePrincipal -TenantId "%azure.TenantId%";
+Select-AzureRmSubscription -SubscriptionId "%azure.SubscriptionId%";
 
-Write-Verbose "In input costsSaver.Downscale we have %costsSaver.Downscale%";
+Write-Host "In input costsSaver.Downscale we have %costsSaver.Downscale%";
 if ("%costsSaver.Downscale%".ToLower() -eq "true") {
     $Downscale = $true;
 } else {
